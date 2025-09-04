76b76727c71b859647c7070b92f81d30380b5212
unknown <cami.rivas.dlb@gmail.com>
2025-09-04 18:51:20 -0400

    push

--- src/index.js
+++ src/index.js
@@ -10,12 +10,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 connectDB();
 
-
-
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}, `));
